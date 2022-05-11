246cc9a706137fd108ea21a96d6f2f6ae1819cec
Jernej Vivod <vivod.jernej@gmail.com>
2022-05-11 18:39:57 +0200

    [#1] Add basic project structure

---
+++ __main__.py
@@ -0,0 +1,10 @@
+import argparse
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='ny-parking-violations-analysis')
+    main()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ny-parking-violations-analysis',
+    version='0.1.0',
+    packages=[''],
+    url='https://github.com/jernejvivod/ny-parking-violations-analysis',
+    license='',
+    author='Jernej Vivod',
+    author_email='vivod.jernej@gmail.com',
+    description='Analysis of parking violations data'
+)
