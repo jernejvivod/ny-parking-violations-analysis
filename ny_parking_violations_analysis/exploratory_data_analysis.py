298fbfbaef0d1eaf3da581256404a7ed2e3a951b
JakobSkornik <skornikjakobs@gmail.com>
2022-05-15 18:26:07 +0200

    [#7] Added group-by-count with plotting

---
+++ ny_parking_violations_analysis/exploratory_data_analysis.py
@@ -0,0 +1,18 @@
+from fileinput import filename
+import dask.dataframe as dd
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def groupby_count(df: dd.DataFrame, col: str) -> None:
+    return (df.groupby(col)
+            .agg({'Summons Number': 'count'})
+            .compute()
+            .sort_values(['Summons Number'], ascending=False)
+            .reset_index(level=0)
+            .rename(columns={'Summons Number': 'Ticket Count'})
+            .head(10))
+
+def plot_bar(df: pd.DataFrame, x: str, y: str, filename: str) -> None:
+    df.plot(x=x, y=y, kind='bar')
+    plt.tight_layout()
+    plt.savefig(fname=f'ny-parking-violations-analysis/img/{filename}')
