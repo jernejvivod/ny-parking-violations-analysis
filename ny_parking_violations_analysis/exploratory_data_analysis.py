97eb6328f92c7edce118ed081e33a535c90ee334
JakobSkornik <skornikjakobs@gmail.com>
2022-05-15 18:35:10 +0200

    [#7] Added more examples

--- ny_parking_violations_analysis/exploratory_data_analysis.py
+++ ny_parking_violations_analysis/exploratory_data_analysis.py
@@ -3,14 +3,14 @@ import dask.dataframe as dd
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def groupby_count(df: dd.DataFrame, col: str) -> None:
+def groupby_count(df: dd.DataFrame, col: str, amt: int=10) -> None:
     return (df.groupby(col)
             .agg({'Summons Number': 'count'})
             .compute()
             .sort_values(['Summons Number'], ascending=False)
             .reset_index(level=0)
             .rename(columns={'Summons Number': 'Ticket Count'})
-            .head(10))
+            .head(amt))
 
 def plot_bar(df: pd.DataFrame, x: str, y: str, filename: str) -> None:
     df.plot(x=x, y=y, kind='bar')
